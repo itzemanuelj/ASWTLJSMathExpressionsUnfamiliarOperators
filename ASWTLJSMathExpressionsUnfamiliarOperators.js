958469855ef0bc19533356df5fe28ade604670a5
Emanuel Joness <itzemanuelj@gmail.com>
2022-08-16 12:44:18 -0400

    inital commit for Math Expressions unfamiliar op.

---
+++ ASWTLJSMathExpressionsUnfamiliarOperators.js
@@ -0,0 +1,78 @@
+// Code the short form of x = x + 1;
+
+x++;
+
+// If x has a value of 100, what is the fastest way to reduce it to 99 with a math expression?
+
+x--;
+
+// var x = 50;
+// var y = x++;
+// What is the value of y?
+
+x = 50;
+
+// var x = 50;
+// var y = --x;
+// What is the value of y?
+
+y = 49;
+
+// In a single statement, increment the variable num and assign its new value to a second variable, newNum, which hasn't been declared beforehand.
+
+var Newnum = ++num;
+
+// In a single statement, decrement num and assign its original value to newNum, which hasn't been declared beforehand.
+
+newNum = num--; 
+
+// Using minimal code, decrement x.
+
+x--;
+
+// What is the long version of x++?
+
+x = x + 1;
+
+// In a single statement subtract 1 from a variable and assign the new value to another variable, which hasn't been declared beforehand.
+
+var cost = 10;
+cost--;
+
+// In a single statement add 1 to a variable and assign its original value to another variable, which hasn't been declared beforehand.
+
+var oneVar = twoVar++;
+
+// Increment a variable. In a second statement, display its new value in an alert.
+
+x++;
+alert(x);
+
+// Time goal: 15 seconds. (The timer starts when you click in the answer field.) Code the short form of x = x + 1;
+
+x++;
+
+// Time goal: 15 seconds. Using minimal code, decrement x.
+
+x--;
+
+// Time goal: 20 seconds. What is the long version of x++?
+
+x = x + 1;
+
+// Time goal: 25 seconds. In a single statement subtract 1 from a variable and assign the new value to another variable, which hasn't been declared beforehand.
+
+var oldVar = --newVar;
+
+// Time goal: 25 seconds. In a single statement add 1 to a variable and assign its original value to another variable, which hasn't been declared beforehand.
+
+var oldVar = newVar++;
+
+// Time goal: 25 seconds. Increment a variable, then display its new value in an alert.
+
+var total = 10;
+total++;
+alert(total);
+
+
+
